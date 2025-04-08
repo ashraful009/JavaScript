5d65b139df4e1ea9ef5f352e86e2fd35a8866290
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-04-08 10:13:10 +0600

    Create logicalAndOr.js

---
+++ Chapter 3 If Else Condition/logicalAndOr.js
@@ -0,0 +1,15 @@
+var name 
+var fullName = name || "John Doe"
+
+console.log(fullName) 
+
+
+
+var Name = "halam"
+var FullName = Name || "John Doe"
+
+console.log(FullName)
+
+var isOk = true
+
+isOk && console.log("ok")

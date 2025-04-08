fd4548017745e618a3396438eb604d10f3e900c3
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-04-08 10:11:26 +0600

    Create typeConversion.js

---
+++ Chapter 2/typeConversion.js
@@ -0,0 +1,23 @@
+var str = '234783'
+var str1 = 10
+console.log(Number(str)) 
+console.log(parseInt(str1))
+
+// Falsy Values
+// 0
+// ""
+// null
+// undefined
+// NaN
+// false
+
+console.log(Boolean(0)) // false
+console.log(Boolean("")) // false
+console.log(Boolean(null)) // false
+console.log(Boolean(undefined)) // false   
+console.log(Boolean(NaN)) // false
+console.log(Boolean(false)) // false
+console.log(Boolean(-Infinity))
+console.log(Boolean(2))
+
+

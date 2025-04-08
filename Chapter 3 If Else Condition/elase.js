d921fd2b51228359476f0421e89175c12c58165a
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-04-08 10:12:15 +0600

    Create elase.js

---
+++ Chapter 3 If Else Condition/elase.js
@@ -0,0 +1,29 @@
+var a  = 20;
+var b = 20;
+
+if(a > b){
+    console.log("A is getter then B");
+}
+else {
+    console.log("B is getter then A");  
+}
+
+
+
+if(a == b){
+    console.log("B and A is equal");
+}
+
+if(a % 2 == 0){
+    console.log("A is even number");
+}
+else{
+    console.log("A is odd number");
+}
+
+
+
+
+
+
+

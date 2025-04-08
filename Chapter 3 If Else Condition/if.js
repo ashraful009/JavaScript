6f578240eb4c38eff177d39fad8c7ea5a5e76d91
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-04-08 10:12:28 +0600

    Create if.js

---
+++ Chapter 3 If Else Condition/if.js
@@ -0,0 +1,22 @@
+var a  = 20;
+var b = 20;
+
+if(a > b){
+    console.log("A is getter then B");
+}
+
+if(a < b){
+    console.log("B is getter then A");
+}
+
+if(a == b){
+    console.log("B and A is equal");
+}
+
+if(a % 2 == 0){
+    console.log("A is even number");
+}
+
+if(a % 2 != 0){
+    console.log("A is odd number");
+}

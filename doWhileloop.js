6ea3a724ede74b889fdffce1e35c2ce8cfc23630
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-04-08 10:13:30 +0600

    Create doWhileloop.js

---
+++ doWhileloop.js
@@ -0,0 +1,24 @@
+// Do While loop
+
+do{
+    console.log("Hello World")
+    }while(true)
+
+
+// Do While loop with condition
+do{
+    if(i % 2 == 0){
+        console.log("Hello World")
+    }
+    i++
+}   while(i < 10)
+// Do While loop with condition and break
+do{
+    if(i % 2 == 0){
+        console.log("Hello World")
+    }
+    else{
+        break
+    }
+    i++                     
+ }while(i < 10)       

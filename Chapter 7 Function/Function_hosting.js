2a38db48ff6f06532b513ea87a6789756c85554d
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-04-11 00:37:41 +0600

    Create Function_hosting.js

---
+++ Chapter 7 Function/Function_hosting.js
@@ -0,0 +1,49 @@
+console.log(age)
+
+var age = 50;
+
+console.log(num)
+
+let num = 50;
+
+let greet = function () {
+    console.log('Greeting')
+}
+
+function greetme (greet, fulname){
+    console.log('hellow', fulname);
+    greet()
+}
+
+greetme(greet, 'hukka')
+
+
+const arr = [
+    function (a, b){
+        return a + b;
+    },
+    function(a, b){
+        return a - b;
+    },
+    function(a, b){
+        return a * b;
+    },
+
+];
+for (let i in arr){
+    console.log(arr[i](5,10));
+}
+
+
+let obj = {
+    age : 25,
+    wet : 3456,
+    ht : 234,
+    greet:()=> {
+        console.log("hello")
+    }
+
+}
+
+console.log(obj.age);
+obj.greet();

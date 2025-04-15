6e158d8c5425df532faee3cf572170b4ce35dda4
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-04-15 11:00:35 +0600

    Create script.js

---
+++ chapter 10 DOM Manipulation/script.js
@@ -0,0 +1,9 @@
+
+let text = document.querySelector("h1");
+console.log(text); // Get the h1 element
+console.log(text.innerHTML); // Get the HTML inside the h1 element
+console.log(text.innerText); // Get the text inside the h1 element
+
+console.log(text.textContent); // Get the text content inside the h1 element
+console.log(text.outerHTML); // Get the HTML inside the h1 element including the h1 tag
+
